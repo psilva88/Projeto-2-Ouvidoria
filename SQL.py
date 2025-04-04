ef3d75138eddf771e860cb35f90c38bb1f7f04c5
psilva88 <arthurpsilva@aluno.colegiomotiva.com.br>
2025-04-04 02:00:31 -0300

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -1,1 +1,8 @@
+CREATE TABLE manifestation (
+    codigo INT AUTO_INCREMENT ,
+    tipo ENUM('Reclamação', 'Sugestão', 'Elogio') NOT NULL,
+    manifestacao VARCHAR(255),
+    PRIMARY KEY (codigo)
+);
 
+SELECT * FROM manifestation;
